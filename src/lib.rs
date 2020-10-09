2ac4db10da1d78cb6ed0078dc278bc3d0e0c2fe6
yangdonglin <332407470@qq.com>
2020-10-09 10:39:40 +0800

    fix warning for an obsolete name

--- src/lib.rs
+++ src/lib.rs
@@ -133,7 +133,7 @@
 //! be separate crates that pull in the dependencies they require.
 
 // Catch documentation errors caused by code changes.
-#![deny(intra_doc_link_resolution_failure)]
+#![deny(broken_intra_doc_links)]
 
 #[cfg(test)]
 #[macro_use]
