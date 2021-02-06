d07c8ee6d97f69a823088275bcb7c05d601ddbaf
yangdonglin <332407470@qq.com>
2021-02-06 18:40:09 +0800

    get rid of locks

--- src/gpu/locks.rs
+++ src/gpu/locks.rs
@@ -1,26 +1,24 @@
-use fs2::FileExt;
+//use fs2::FileExt;
 use log::{debug, info, warn};
-use std::fs::File;
-use std::path::PathBuf;
+//use std::fs::File;
+//use std::path::PathBuf;
 
-const GPU_LOCK_NAME: &str = "bellman.gpu.lock";
-const PRIORITY_LOCK_NAME: &str = "bellman.priority.lock";
-fn tmp_path(filename: &str) -> PathBuf {
-    let mut p = std::env::temp_dir();
-    p.push(filename);
-    p
-}
+// const GPU_LOCK_NAME: &str = "bellman.gpu.lock";
+// const PRIORITY_LOCK_NAME: &str = "bellman.priority.lock";
+//fn tmp_path(filename: &str) -> PathBuf {
+//    let mut p = std::env::temp_dir();
+//    p.push(filename);
+//    p
+//}
 
 /// `GPULock` prevents two kernel objects to be instantiated simultaneously.
 #[derive(Debug)]
-pub struct GPULock(File);
+pub struct GPULock();
 impl GPULock {
     pub fn lock() -> GPULock {
         debug!("Acquiring GPU lock...");
-        let f = File::create(tmp_path(GPU_LOCK_NAME)).unwrap();
-        f.lock_exclusive().unwrap();
         debug!("GPU lock acquired!");
-        GPULock(f)
+        GPULock()
     }
 }
 impl Drop for GPULock {
@@ -34,29 +32,17 @@ impl Drop for GPULock {
 /// signaling all other processes to release their `GPULock`s.
 /// Only one process can have the `PriorityLock` at a time.
 #[derive(Debug)]
-pub struct PriorityLock(File);
+pub struct PriorityLock();
 impl PriorityLock {
     pub fn lock() -> PriorityLock {
         debug!("Acquiring priority lock...");
-        let f = File::create(tmp_path(PRIORITY_LOCK_NAME)).unwrap();
-        f.lock_exclusive().unwrap();
         debug!("Priority lock acquired!");
-        PriorityLock(f)
+        PriorityLock()
     }
-    pub fn wait(priority: bool) {
-        if !priority {
-            File::create(tmp_path(PRIORITY_LOCK_NAME))
-                .unwrap()
-                .lock_exclusive()
-                .unwrap();
-        }
+    pub fn wait(_priority: bool) {
     }
-    pub fn should_break(priority: bool) -> bool {
-        !priority
-            && File::create(tmp_path(PRIORITY_LOCK_NAME))
-                .unwrap()
-                .try_lock_exclusive()
-                .is_err()
+    pub fn should_break(_priority: bool) -> bool {
+        false
     }
 }
 impl Drop for PriorityLock {
