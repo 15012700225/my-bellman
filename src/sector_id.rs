358cae3d58903256d3e547c504f9bfea957b3f6f
yangdonglin <332407470@qq.com>
2021-02-23 16:17:06 +0800

    add sector_id for window post

---
+++ src/sector_id.rs
@@ -0,0 +1,13 @@
+
+use lazy_static::lazy_static;
+#[derive(Copy, Clone, Debug)]
+pub struct SectorId(u64);
+
+lazy_static! {
+    pub static ref SECTOR_ID: SectorId = SectorId(
+        std::env::args()
+            .nth(4)
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(0)
+    );
+}
